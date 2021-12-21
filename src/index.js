124a742d027eefe2657b6f0364ef35a994a85100
Kristofer Gullberg <kristofer.gullberg@teliacompany.com>
2021-12-21 20:18:11 +0100

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,36 +1,51 @@
 'use strict';
+
 const inquirer = require('inquirer');
 const shuffleArray = require('./shuffle');
-const teamData = require("./config/team.json");
+let teamData = require("./config/team.json");
 
 const commonConfig = {
   type: 'checkbox',
   pageSize: 10,
 }
 
+let promptQuestions = [];
+
+function promptSetup() {
+  if(teamData.developers) {
+    promptQuestions = [...promptQuestions,
+      {
+        ...commonConfig,
+        message: 'Select developers',
+        name: 'developers',
+        choices: [
+          new inquirer.Separator(' = Developers = '),
+          ...teamData.developers
+        ],
+      }
+    ];
+  }
+  if(teamData.others) {
+    promptQuestions = [...promptQuestions,
+      {
+        ...commonConfig,
+        message: 'Select others',
+        name: 'others',
+        choices: [
+          new inquirer.Separator(' = Others = '),
+          ...teamData.others
+        ],
+      }
+    ];
+  }
+
+  return promptQuestions;
+}
+
 inquirer
-  .prompt([
-    {
-      ...commonConfig,
-      message: 'Select developers',
-      name: 'developers',
-      choices: [
-        new inquirer.Separator(' = Developers = '),
-        ...teamData.developers
-      ],
-    },
-    {
-      ...commonConfig,
-      message: 'Select others',
-      name: 'others',
-      choices: [
-        new inquirer.Separator(' = Others = '),
-        ...teamData.others
-      ],
-    },
-  ])
+  .prompt(promptSetup())
   .then((answers) => {
-    console.log(shuffleArray(answers.developers));
-    console.log('+');
-    console.log(shuffleArray(answers.others));
+    for(const key in answers) {
+      console.log(shuffleArray(answers[key]));
+    }
   });
